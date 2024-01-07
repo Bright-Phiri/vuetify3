435f0c01a1b796fd4cc0418f35d97553458a48cd
Bright Phiri <bphiri.aki@gmail.com>
2024-01-07 14:35:08 +0200

    final touches on side bar

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,16 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: NavBar
+    component: NavBar,
+    children: [
+      {
+        path: "",
+        name: "dashboard",
+        component: () => import("../views/HomeView.vue")
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  
 ]
 
 const router = createRouter({
