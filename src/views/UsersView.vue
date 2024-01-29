e67a2e9e8964b60ed1beeba4e9212f234a7a7a42
Bright Phiri <bphiri.aki@gmail.com>
2024-01-29 21:26:19 +0200

    wip

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -7,8 +7,8 @@
           <v-card-title class="d-flex justify-space-between">
             New User
             <v-icon icon="mdi-close" size="small" v-on:click="addUserDialog = ! addUserDialog"/>
-          </v-card-title>
-          <v-card-text>
+          </v-card-title> 
+          <v-card-text> 
             <v-form>
               <v-row>
                 <v-col cols="12" sm="6">
