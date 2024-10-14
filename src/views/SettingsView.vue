880654c5e91e5c330ee913bcff30fb8b1dd14801
Bright Issah <bphiri.aki@gmail.com>
2024-10-14 10:44:08 +0200

    WIP

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -2,40 +2,34 @@
   <v-row class="px-12">
     <v-col cols="8">
       <v-card elevation="2">
-        <v-card-title>
-          Personal Information
-        </v-card-title>
+        <v-card-title>Personal Information</v-card-title>
         <v-card-text>
           <v-row class="mt-4">
             <v-col cols="6">
-              <v-text-field label="First Name" variant="outlined" density="compact"
-              prepend-inner-icon="mdi-account"></v-text-field>
+              <v-text-field label="First Name" variant="outlined" density="compact" prepend-inner-icon="mdi-account"></v-text-field>
             </v-col>
+
             <v-col cols="6">
-              <v-text-field label="Last Name" variant="outlined" density="compact"
-              prepend-inner-icon="mdi-account"></v-text-field>
+              <v-text-field label="Last Name" variant="outlined" density="compact" prepend-inner-icon="mdi-account"></v-text-field>
             </v-col>
           </v-row>
 
           <v-row class="mt-4">
             <v-col cols="6">
-              <v-text-field label="Username" variant="outlined" density="compact"
-                prepend-inner-icon="mdi-account"></v-text-field>
+              <v-text-field label="Username" variant="outlined" density="compact" prepend-inner-icon="mdi-account"></v-text-field>
             </v-col>
+
             <v-col cols="6">
-              <v-text-field label="Email Address" variant="outlined" density="compact"
-                prepend-inner-icon="mdi-email"></v-text-field>
+              <v-text-field label="Email Address" variant="outlined" density="compact" prepend-inner-icon="mdi-email"></v-text-field>
             </v-col>
           </v-row>
 
           <v-row class="mt-4">
             <v-col cols="6">
-              <v-text-field label="Phone Number" variant="outlined" density="compact"
-                prepend-inner-icon="mdi-phone"></v-text-field>
+              <v-text-field label="Phone Number" variant="outlined" density="compact" prepend-inner-icon="mdi-phone"></v-text-field>
             </v-col>
             <v-col cols="6">
-              <v-text-field label="Password" variant="outlined" density="compact"
-                prepend-inner-icon="mdi-lock"></v-text-field>
+              <v-text-field label="Password" variant="outlined" density="compact" prepend-inner-icon="mdi-lock"></v-text-field>
             </v-col>
           </v-row>
         </v-card-text>
@@ -51,6 +45,7 @@
         <v-card-title>
           Security Information
         </v-card-title>
+
         <v-card-text>
           <v-row class="mt-4">
             <v-col cols="12">
@@ -108,7 +103,6 @@
       }
     }
   }
-
 </script>
 
 <style scoped>
