4101b19a6fb3e5dae05095a5807bcf7153a867f8
Bright Issah <bphiri.aki@gmail.com>
2024-10-16 16:05:08 +0200

    Add sub list to nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,37 +1,32 @@
 <template>
   <div class="NavBar">
-     <v-dialog max-width="540" persistent v-model="logoutDialog">
-       <v-card>
-         <v-card-title class="d-flex justify-space-between text-h6 font-height-light">Log out <v-icon color="red" icon="mdi-close" @click="logoutDialog = !logoutDialog"></v-icon> </v-card-title>
-         <v-divider></v-divider>
-         <v-card-text>Are you sure you would like to logout?</v-card-text>
-         <v-divider></v-divider>
-         <v-card-actions class="d-flex justify-end mb-2">
-           <v-btn variant="outlined" class="text-capitalize px-3" color="black" rounded="xl" @click="logoutDialog = !logoutDialog">Close</v-btn>
-           <v-btn variant="flat" class="text-capitalize px-5" color="blue" @click="logout" rounded="xl">Logout</v-btn>
-         </v-card-actions>
-       </v-card>
-     </v-dialog>
+    <v-dialog max-width="540" persistent v-model="logoutDialog">
+      <v-card>
+        <v-card-title class="d-flex justify-space-between text-h6 font-height-light">Log out <v-icon color="red"
+            icon="mdi-close" @click="logoutDialog = !logoutDialog"></v-icon> </v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>Are you sure you would like to logout?</v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions class="d-flex justify-end mb-2">
+          <v-btn variant="outlined" class="text-capitalize px-3" color="black" rounded="xl"
+            @click="logoutDialog = !logoutDialog">Close</v-btn>
+          <v-btn variant="flat" class="text-capitalize px-5" color="blue" @click="logout" rounded="xl">Logout</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-app-bar flat color="#2D3541">
-     <v-app-bar-nav-icon v-on:click="drawer = !drawer"></v-app-bar-nav-icon>
-     <v-app-bar-title class="text-h7 font-weight-thin">Block Managment Information System</v-app-bar-title>
-     <v-text-field
-        v-model="searchText"
-        class="ml-10 mt-5 text-white"
-        placeholder="Enter ID. i.e Block ID or TPIN"
-        variant="outlined"
-        density="compact"
-        size="small"
-        append-inner-icon="mdi-magnify"
-      ></v-text-field>
+      <v-app-bar-nav-icon v-on:click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title class="text-h7 font-weight-thin">Block Managment Information System</v-app-bar-title>
+      <v-text-field v-model="searchText" class="ml-10 mt-5 text-white" placeholder="Enter ID. i.e Block ID or TPIN"
+        variant="outlined" density="compact" size="small" append-inner-icon="mdi-magnify"></v-text-field>
       <v-spacer></v-spacer>
-     <template v-slot:append>
-       <div class="d-flex">
-         <v-icon class="mt-3" icon="mdi-account-circle"></v-icon>
-         <span class="mt-3 ml-1">biphiri</span>
-         <v-btn icon="mdi-logout" @click="showLogoutDialog"></v-btn>
-       </div>
-     </template>
+      <template v-slot:append>
+        <div class="d-flex">
+          <v-icon class="mt-3" icon="mdi-account-circle"></v-icon>
+          <span class="mt-3 ml-1">biphiri</span>
+          <v-btn icon="mdi-logout" @click="showLogoutDialog"></v-btn>
+        </div>
+      </template>
     </v-app-bar>
     <v-navigation-drawer color="#395BB6" rail :rail-width="45" permanent floating>
       <v-list>
@@ -39,12 +34,27 @@
         <v-list-item prepend-icon="mdi-home-outline" to="/dashbaord"></v-list-item>
       </v-list>
     </v-navigation-drawer>
-  
+
     <v-navigation-drawer :width="245" v-model="drawer" floating>
       <v-list nav>
         <v-list-item-subtitle class="mt-4">MAIN MENU</v-list-item-subtitle>
         <v-divider class="my-3"></v-divider>
-        <v-list-item v-for="item in menuItems" :key="item.text" :prepend-icon="item.icon" :title="item.text" :to="item.to"></v-list-item>
+        <!-- Loop through menu items -->
+        <template v-for="item in menuItems" :key="item.text">
+          <!-- If item has children, render as a collapsible group (v-list-group) -->
+          <v-list-group v-if="item.children" :value="item.text">
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" :title="item.text" :prepend-icon="item.icon"></v-list-item>
+            </template>
+
+            <!-- Render sub-items (children) -->
+            <v-list-item v-for="(subItem, i) in item.children" :key="i" :prepend-icon="subItem.icon"
+              :title="subItem.text" :to="subItem.to"></v-list-item>
+          </v-list-group>
+
+          <!-- Render regular list items without children -->
+          <v-list-item v-else :prepend-icon="item.icon" :title="item.text" :to="item.to"></v-list-item>
+        </template>
       </v-list>
     </v-navigation-drawer>
     <Transition name="slide-fade" mode="in-out">
@@ -69,7 +79,16 @@ export default {
       {
         text: "Block Info",
         icon: "mdi-information",
-        to: "/blocks",
+        children: [
+          {
+            text: "Manage Blocks",
+            to: "/blocks",
+          },
+          {
+            text: "Assign Taxpayer to Block",
+            to: "/blocks/history",
+          }
+        ]
       },
       {
         text: "Presumptive Tax",
@@ -97,7 +116,7 @@ export default {
         to: "/settings",
       },
       {
-        text: "Check Id- Print Queu",
+        text: "Check Id- Print Queue",
         icon: "mdi-checkbox-marked",
         to: "/settings",
       },
@@ -107,14 +126,15 @@ export default {
         to: "/settings",
       },
     ];
+
     provide('drawerState', drawer);
 
     const showLogoutDialog = () => {
       logoutDialog.value = true
     }
 
-    const logout = ()=> {
-      router.push( { path: '/login' })
+    const logout = () => {
+      router.push({ path: '/login' })
     };
 
     return {
